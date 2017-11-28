6c85b7ada238e291c347440874e58276e3b4c0db
yuejunjie <yjj2012uestc@163.com>
2017-11-28 10:31:31 +0800

    第一次提交

---
+++ js/App.js
@@ -0,0 +1,25 @@
+var App = angular.module('App',[
+    'ngRoute'
+])
+    .config(
+        ['$routeProvider',function ($routeProvider) {
+            $routeProvider.when('/',{
+                templateUrl:'template/home.html'
+            });
+            $routeProvider.when('/cart',{
+                templateUrl:'template/cart.html'
+            });
+            $routeProvider.when('/sort',{
+                templateUrl:'template/sort.html'
+            });
+            $routeProvider.when('/nearby',{
+                templateUrl:'template/nearby.html'
+            });
+            $routeProvider.when('/message',{
+                templateUrl:'template/message.html'
+            });
+            $routeProvider.otherwise({
+                templateUrl:'template/home.html'
+            })
+        }]
+    );
---
+++ js/controller/AppCtrl.js
@@ -0,0 +1,3 @@
+App.controller('AppCtrl',['$scope',function ($scope) {
+
+}]);
